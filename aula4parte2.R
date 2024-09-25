a50658518cb0f357aa927d6635bd97437747b51f
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-09-24 23:15:06 -0300

    update

---
+++ aula4parte2.R
@@ -0,0 +1,17 @@
+#ler csv de uma url
+url = "https://www-usr.inf.ufsm.br/~joaquim/UFSM/DM/ds/LD00c.csv"
+dados = read.csv(url)
+head(dados)
+
+library(ggplot2)
+
+precipitacao = dados$Precipitacao
+tempMaxima = dados$TempMaxima
+tempMinima = dados$TempMinima
+insolacao = dados$Insolacao
+evaporacaoPiche = dados$Evaporacao.Piche
+tempCompMedia = dados$Temp.Comp.Media
+umidadeRelativaMedia = dados$Umidade.Relativa.Media
+velocidadedoVentoMedia = dados$Velocidade.do.Vento.Media
+
+summary(dados)
